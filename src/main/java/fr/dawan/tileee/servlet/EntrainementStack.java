04bcdd5719e809926a2336d8ae35156c4ddd2836
Maxime Deschodt <maximedeschodt@gmail.com>
2019-12-31 14:33:39 +0100

    tentative de migration vers SpringMVC

--- src/main/java/fr/dawan/tileee/servlet/EntrainementStack.java
+++ src/main/java/fr/dawan/tileee/servlet/EntrainementStack.java
@@ -18,34 +18,15 @@ import fr.dawan.tileee.dao.CardDao;
 import fr.dawan.tileee.service.TagsService;
 
 @Controller
-@RequestMapping("tileee")
 public class EntrainementStack {
 
-	@Autowired
-	private TagsService service;
-	
-	private String displayPag(Integer page, Integer max,  Model model) {
-		int start = (page - 1) * max;
-		List<Tag> ls = service.pagination(start, max);
-		model.addAttribute("lst", ls);
-		
-		boolean suivExist = (page*max) < service.count();
-		System.out.println(service.count());
-		model.addAttribute("suivExist", suivExist);
-		model.addAttribute("page", page);
-		model.addAttribute("max", max);
-		
-		return "entrainement";	
-	}
-	
 	@GetMapping("/EntrainementStack/{tag}")
-	public String pagination(@PathVariable("tag") String tag, Model model) {
+	public String pagination(@PathVariable("tag") String tag, Model model, HttpSession session) {
 		CardDao carddao = new CardDao();
-		HttpSession session = model.getSession();
-		User user = (User) session.getAttribute("user");
 		
+		User user = (User) session.getAttribute("user");
 		List<Card> listCard = carddao.findByTag(user, tag, true);
-		session.setAttribute()
-		return displayPag(page, TagPerPage, model);
+		model.addAttribute("lC", listCard);
+		return "entrainementstack";
 	}
 }
